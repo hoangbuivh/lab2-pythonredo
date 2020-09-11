2a02f0ffbb5217a0ff91e826f1b09f9a19fac158
Hoang  Bui <hhb5051@psu.edu>
2020-09-11 14:54:03 +0000

    final commit

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 def getLetterGrade(scoreGrade):
-  scoreGrade = float(input("Enter your CMPSC 131 grade: "))
-  letterGrade = ""
   if scoreGrade >= 93.0: 
-    letterGrade = "A"
+    return "A"
   elif scoreGrade <= 93.0 and scoreGrade >= 90.0: 
-    letterGrade = "A-"
+    return "A-"
   elif scoreGrade <= 90.0 and scoreGrade >= 87.0: 
-    letterGrade = "B+"
+    return "B+"
   elif scoreGrade <= 87.0 and scoreGrade >= 83.0: 
-    letterGrade = "B"
+    return "B"
   elif scoreGrade <= 83.0 and scoreGrade >= 80.0: 
-    letterGrade = "B-"
+    return "B-"
   elif scoreGrade <= 80.0 and scoreGrade >= 77.0: 
-    letterGrade = "C+"
+    return "C+"
   elif scoreGrade <= 77.0 and scoreGrade >= 70.0: 
-    letterGrade = "C"
+    return "C"
   elif scoreGrade <= 70.0 and scoreGrade >= 60.0: 
-    letterGrade = "D"
+    return "D"
   else: 
-    letterGrade = "F"
-  print(f"Your letter grade for CMPSC 131 is {letterGrade}. ")
+    return "F"
+  
+def run(): 
+  entrance = input("Enter your CMPSC 131 grade: ")
+  print("Your letter grade for CMPSC 131 is ",getLetterGrade(entrance),".")
 
-  if __name__== "__main__":
-    getLetterGrade(scoreGrade)
+if __name__== "__main__":
+  run()
